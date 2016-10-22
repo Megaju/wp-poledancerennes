e5d9633ed5455fff26ecdb5ccaf58b13cb09ef81
Megaju <julien_malle@outlook.fr>
2016-10-22 16:10:08 +0100

    ajout fonction modif image header

--- functions.php
+++ functions.php
@@ -8,3 +8,13 @@ register_nav_menus( array(
     ) );
 
 ?>
+
+<?php
+    $args = array(
+    'width'         => 800,
+    'height'        => 540,
+    'default-image' => get_template_directory_uri() . '/img/facade.JPG',
+    'uploads'       => true,
+    );
+add_theme_support( 'custom-header', $args );
+?>
