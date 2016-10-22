277ba8007ff0ce8858c909fcb9fb84f7342fc6c9
Megaju <julien_malle@outlook.fr>
2016-10-22 15:38:50 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# wp-poledancerennes" 
---
+++ functions.php
@@ -0,0 +1,1 @@
+<?php if ( function_exists('register_sidebar') ) register_sidebar(); ?><!-- widget sidebar - indiquer le nombres de colones dans register_sidebar(ici), ne rien mettre si < 2 colones -->
