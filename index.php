727f5e9fe825ba55de8af63a91f31ce414fe8531
Megaju <julen_malle@outlook.fr>
2016-11-05 21:03:22 +0100

    debut de hover

--- index.php
+++ index.php
@@ -44,7 +44,41 @@
         <?php wd_slider(1); ?>
         <span class="more-on-fb">Encore plus d'images sur facebook. <i class="fa fa-facebook-official"></i></span>
     </div>
-
+    
+    <!-- SECTION DES EVENEMENTS -->
+    <section class="event-section">
+        <div class="event-content">
+        <?php query_posts('category_name=evenement'); ?>
+            <?php if(have_posts()) : ?>
+                <?php $counter =0; ?>
+                    <?php while(have_posts() && $counter<9) : the_post();?>
+                        <?php $counter++ ?>
+                            <!-- affichage des articles -->
+                            <div class="article-event">
+                                <!-- image -->
+                                <div class="event-img">
+                                    <?php the_post_thumbnail(array(250,250));?>
+                                </div>
+                                
+                                <!-- partie invisible pour le hover -->
+                                <div class="event-hover">
+                                    <div id="post-<?php the_ID(); ?>" class="post event_content">
+                                        <!-- title -->
+                                        <h2><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
+                                        <!-- separator -->
+                                        <div class="separator-purple"></div>
+                                        <!-- article text -->
+                                        <p class="postmetadata">
+                                            <?php the_content(__('<i class="fa fa-3x fa-plus-circle read-more"></i>')); ?>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                <?php endwhile; ?>
+            <?php endif; ?>                   
+        </div>
+                                
+    </section>
 
     <?php get_footer(); ?>
 
