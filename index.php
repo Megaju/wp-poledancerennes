28479337337321c4736db1150c8eeba8f59e0584
Megaju <julen_malle@outlook.fr>
2016-11-05 18:51:28 +0100

    image article ok, problème de ratio à revoir

--- index.php
+++ index.php
@@ -2,33 +2,38 @@
 
     <!-- SECTION DES COURS DE DANCE -->
     <section class="dance-classes">
+                            <div class="content">
         <?php query_posts('category_name=coursDance'); ?>
             <?php if(have_posts()) : ?>
                 <?php $counter =0; ?>
                     <?php while(have_posts() && $counter<3) : the_post();?>
                         <?php $counter++ ?>
 
-                            <div class="content">
+                               
                                 <div class="ligne-design">
                                     <div></div>
                                 </div>
+                                <!-- affichage des articles -->
                                 <div class="article-dance">
-                                    <!-- affichage des articles -->
-                                    <h2><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
-                                    <div class="separator-purple"></div>
-                                    <div class="post" id="post-<?php the_ID(); ?>">
-                                        <div class="post_content">
-                                            <?php the_post_thumbnail(array(250,250));?>
-                                                <p class="postmetadata">
-                                                    <?php the_content(__('<i class="fa fa-3x fa-plus-circle read-more"></i>')); ?>
-                                                </p>
-                                        </div>
+                                    <!-- image -->
+                                    <div class="article-img">
+                                        <?php the_post_thumbnail(array(250,250));?>
+                                    </div>
+                                    <div id="post-<?php the_ID(); ?>" class="post post_content">
+                                        <!-- title -->
+                                        <h2><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
+                                        <!-- separator -->
+                                        <div class="separator-purple"></div>
+                                        <!-- article text -->
+                                        <p class="postmetadata">
+                                            <?php the_content(__('<i class="fa fa-3x fa-plus-circle read-more"></i>')); ?>
+                                        </p>
                                     </div>
                                 </div>
                                 <?php endwhile; ?>
                             <?php endif; ?>
                                       
-                                </div>
+                            </div>
                                 
                                  <div class="ligne-design-button">
                                     <div></div>
