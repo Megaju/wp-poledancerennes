f4a4ddcbc279ff6052ab6497a7b8e7a5471ee726
Megaju <julen_malle@outlook.fr>
2016-11-05 19:00:38 +0100

    bug sur les lignes violettes ok

--- index.php
+++ index.php
@@ -2,43 +2,40 @@
 
     <!-- SECTION DES COURS DE DANCE -->
     <section class="dance-classes">
-                            <div class="content">
+        <div class="content">
         <?php query_posts('category_name=coursDance'); ?>
             <?php if(have_posts()) : ?>
                 <?php $counter =0; ?>
                     <?php while(have_posts() && $counter<3) : the_post();?>
                         <?php $counter++ ?>
-
-                               
-                                <div class="ligne-design">
-                                    <div></div>
+                        <div class="row-article">
+                            <!-- ligne violette à gauche des articles -->
+                            <div class="ligne-design"><div></div></div>
+                            
+                            <!-- affichage des articles -->
+                            <div class="article-dance">
+                                <!-- image -->
+                                <div class="article-img">
+                                    <?php the_post_thumbnail(array(250,250));?>
                                 </div>
-                                <!-- affichage des articles -->
-                                <div class="article-dance">
-                                    <!-- image -->
-                                    <div class="article-img">
-                                        <?php the_post_thumbnail(array(250,250));?>
-                                    </div>
-                                    <div id="post-<?php the_ID(); ?>" class="post post_content">
-                                        <!-- title -->
-                                        <h2><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
-                                        <!-- separator -->
-                                        <div class="separator-purple"></div>
-                                        <!-- article text -->
-                                        <p class="postmetadata">
-                                            <?php the_content(__('<i class="fa fa-3x fa-plus-circle read-more"></i>')); ?>
-                                        </p>
-                                    </div>
+                                <div id="post-<?php the_ID(); ?>" class="post post_content">
+                                    <!-- title -->
+                                    <h2><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
+                                    <!-- separator -->
+                                    <div class="separator-purple"></div>
+                                    <!-- article text -->
+                                    <p class="postmetadata">
+                                        <?php the_content(__('<i class="fa fa-3x fa-plus-circle read-more"></i>')); ?>
+                                    </p>
                                 </div>
-                                <?php endwhile; ?>
-                            <?php endif; ?>
-                                      
                             </div>
+                        </div>
+                <?php endwhile; ?>
+            <?php endif; ?>                   
+        </div>
                                 
-                                 <div class="ligne-design-button">
-                                    <div></div>
-                                </div>
-                                <a href="#"><button type="submit">En SAVOIR plus</button></a>
+        <div class="ligne-design-button"><div></div></div>
+        <a href="#"><button type="submit">En SAVOIR plus</button></a>
                                 
     </section>
 
